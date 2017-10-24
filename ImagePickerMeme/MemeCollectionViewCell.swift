0224ee5e013c97deeb2302f25d5aa0554052542f
Christian Villere <Christianvillere@gmail.com>
2017-10-24 07:20:00 -0700

    Fix imageView issue in CollectionViewController

---
+++ ImagePickerMeme/MemeCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MemeCollectionViewCell.swift
+//  ImagePickerMeme
+//
+//  Created by Christian Villere on 10/24/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//MARK: memeCollectionViewCell: UICollectionViewCell
+
+class memeCollectionViewCell: UICollectionViewCell {
+    
+    
+    //MARK: outlets
+    
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+    
+    
+    
+}
